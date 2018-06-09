af76340be6b05f86d4614593dad4acf950bddc12
Elijah Manor <elijah.manor@gmail.com>
2018-06-09 18:29:41 -0500

    Removed node_modules

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 #!/usr/bin/env node
 
-console.log( JSON.stringify( require( './me.json' ), null, 3 ) );
+const lolcatejs = require( "lolcatjs" );
+
+lolcatjs.options.seed = Math.round( Math.random() * 1000 );
+lolcatjs.options.colors = true;
+
+const me = JSON.stringify( require( './me.json' ), null, 2 );
+
+console.log( lolcatjs.fromString( me ) );

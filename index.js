52f3c9954d7e81750b11f7d26fc662d509bca9cd
Elijah Manor <elijah.manor@gmail.com>
2018-12-22 22:20:39 -0600

    Change to card look

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 #!/usr/bin/env node
 
-console.log( JSON.stringify( require( './me.json' ), null, 3 ) );
+const chalk = require("chalk");
+const boxen = require("boxen");
+
+const options = {
+	padding: 1,
+	margin: 1,
+  borderStyle: "round",
+  borderColor: "green"
+};
+
+const output = `                 ${chalk.yellow.bold('Elijah Manor')}
+
+👪 Christian and Family Man
+👨‍💻 Senior Front-End Dev at @planview @leankit
+📹 Makes videos at @eggheadio @pluralsight
+
+🐦     https://${chalk.blue("twitter")}.com/${chalk.yellow("elijahmanor")}
+📝                 https://${chalk.yellow("elijahmanor")}.com
+🐙      https://${chalk.blue("github")}.com/${chalk.yellow("elijahmanor")}
+🔗    https://${chalk.blue("linkedin")}.com/${chalk.yellow("elijahmanor")}
+📧                         ${chalk.yellow("elijahmanor")}@${chalk.blue("gmail")}.com
+🖥                    $ npx ${chalk.yellow("elijahmanor")}`;
+
+console.log(chalk.white(boxen(output, options)));

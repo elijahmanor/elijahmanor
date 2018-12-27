ae00c9d07aa1587a6a3001f659e4eff218b7a1fb
Elijah Manor <elijah.manor@gmail.com>
2018-12-27 09:46:54 -0600

    Make Elijah Manor font special

--- index.js
+++ index.js
@@ -9,17 +9,21 @@ const options = {
   borderColor: "green"
 };
 
-const output = `                 ${chalk.yellow.bold('Elijah Manor')}
+let output = `
+        ${chalk.yellow.bold('╔═╗┬  ┬ ┬┌─┐┬ ┬  ╔╦╗┌─┐┌┐┌┌─┐┬─┐')}
+        ${chalk.yellow.bold('║╣ │  │ │├─┤├─┤  ║║║├─┤││││ │├┬┘')}
+        ${chalk.yellow.bold('╚═╝┴─┘┴└┘┴ ┴┴ ┴  ╩ ╩┴ ┴┘└┘└─┘┴└─')}
 
-👪 Christian and Family Man
-👨‍💻 Senior Front-End Dev at @planview @leankit
-📹 Makes videos at @eggheadio @pluralsight
+∙ Christian and Family Man
+∙ Senior Front-End Dev at @planview @leankit
+∙ Makes videos at @eggheadio @pluralsight
 
 🐦     https://${chalk.blue("twitter")}.com/${chalk.yellow("elijahmanor")}
 📝                 https://${chalk.yellow("elijahmanor")}.com
-🐙      https://${chalk.blue("github")}.com/${chalk.yellow("elijahmanor")}
+🐙      https://${chalk.blue("github")}.com/${chalk.yellow("elijahmanor")} 
 🔗    https://${chalk.blue("linkedin")}.com/${chalk.yellow("elijahmanor")}
 📧                         ${chalk.yellow("elijahmanor")}@${chalk.blue("gmail")}.com
 🖥                    $ npx ${chalk.yellow("elijahmanor")}`;
 
-console.log(chalk.white(boxen(output, options)));
+const [firstLine, ...trimmed] = output.split('\n');
+console.log(chalk.white(boxen(trimmed.join('\n'), options)));

3f6eec046195a2848671a02d37de87c9decae510
Elijah Manor <elijah.manor@gmail.com>
2018-12-27 09:53:57 -0600

    Take out emojis so works on more terminals

--- index.js
+++ index.js
@@ -10,20 +10,20 @@ const options = {
 };
 
 let output = `
-        ${chalk.yellow.bold('╔═╗┬  ┬ ┬┌─┐┬ ┬  ╔╦╗┌─┐┌┐┌┌─┐┬─┐')}
-        ${chalk.yellow.bold('║╣ │  │ │├─┤├─┤  ║║║├─┤││││ │├┬┘')}
-        ${chalk.yellow.bold('╚═╝┴─┘┴└┘┴ ┴┴ ┴  ╩ ╩┴ ┴┘└┘└─┘┴└─')}
+      ${chalk.yellow.bold('╔═╗┬  ┬ ┬┌─┐┬ ┬  ╔╦╗┌─┐┌┐┌┌─┐┬─┐')}
+      ${chalk.yellow.bold('║╣ │  │ │├─┤├─┤  ║║║├─┤││││ │├┬┘')}
+      ${chalk.yellow.bold('╚═╝┴─┘┴└┘┴ ┴┴ ┴  ╩ ╩┴ ┴┘└┘└─┘┴└─')}
 
 ∙ Christian and Family Man
 ∙ Senior Front-End Dev at @planview @leankit
 ∙ Makes videos at @eggheadio @pluralsight
 
-🐦     https://${chalk.blue("twitter")}.com/${chalk.yellow("elijahmanor")}
-📝                 https://${chalk.yellow("elijahmanor")}.com
-🐙      https://${chalk.blue("github")}.com/${chalk.yellow("elijahmanor")} 
-🔗    https://${chalk.blue("linkedin")}.com/${chalk.yellow("elijahmanor")}
-📧                         ${chalk.yellow("elijahmanor")}@${chalk.blue("gmail")}.com
-🖥                    $ npx ${chalk.yellow("elijahmanor")}`;
+  https://${chalk.blue("twitter")}.com/${chalk.yellow("elijahmanor")}
+              https://${chalk.yellow("elijahmanor")}.com
+   https://${chalk.blue("github")}.com/${chalk.yellow("elijahmanor")} 
+ https://${chalk.blue("linkedin")}.com/${chalk.yellow("elijahmanor")}
+                      ${chalk.yellow("elijahmanor")}@${chalk.blue("gmail")}.com
+                $ npx ${chalk.yellow("elijahmanor")}`;
 
 const [firstLine, ...trimmed] = output.split('\n');
 console.log(chalk.white(boxen(trimmed.join('\n'), options)));

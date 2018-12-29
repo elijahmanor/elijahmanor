ed3c1dbc35302d6fe7442c27f5f2fe80580b9949
Elijah Manor <elijah.manor@gmail.com>
2018-12-28 22:48:46 -0600

    Prebuild output and console log that

--- index.js
+++ index.js
@@ -1,29 +1,6 @@
 #!/usr/bin/env node
-
-const chalk = require("chalk");
-const boxen = require("boxen");
-const options = {
-	padding: 1,
-	margin: 1,
-  borderStyle: "round",
-  borderColor: "green"
-};
-
-let output = `
-      ${chalk.yellow.bold('╔═╗┬  ┬ ┬┌─┐┬ ┬  ╔╦╗┌─┐┌┐┌┌─┐┬─┐')}
-      ${chalk.yellow.bold('║╣ │  │ │├─┤├─┤  ║║║├─┤││││ │├┬┘')}
-      ${chalk.yellow.bold('╚═╝┴─┘┴└┘┴ ┴┴ ┴  ╩ ╩┴ ┴┘└┘└─┘┴└─')}
-
-∙ Christian and Family Man
-∙ Senior Front-End Dev at @planview @leankit
-∙ Makes videos at @eggheadio @pluralsight
-
-  https://${chalk.blue("twitter")}.com/${chalk.yellow("elijahmanor")}
-              https://${chalk.yellow("elijahmanor")}.com
-   https://${chalk.blue("github")}.com/${chalk.yellow("elijahmanor")} 
- https://${chalk.blue("linkedin")}.com/${chalk.yellow("elijahmanor")}
-                      ${chalk.yellow("elijahmanor")}@${chalk.blue("gmail")}.com
-                $ npx ${chalk.yellow("elijahmanor")}`;
-
-const [firstLine, ...trimmed] = output.split('\n');
-console.log(chalk.white(boxen(trimmed.join('\n'), options)));
+ 
+const fs = require("fs");
+const path = require('path');
+const output = fs.readFileSync(path.join(__dirname, 'bin/output'), 'utf8');
+console.log(output);
